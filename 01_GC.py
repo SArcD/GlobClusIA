fc6d5351138c83eb66e49fc2b8df3de316a37fdb
SArcD <141703135+SArcD@users.noreply.github.com>
2023-09-02 17:24:23 -0600

    Update 01_GC.py

--- 01_GC.py
+++ 01_GC.py
@@ -11,22 +11,49 @@ st.title("Visualizador de Archivos CSV en GitHub")
 repo_url = st.text_input("Introduce la URL del repositorio de GitHub:", "")
 
 # Función para obtener la lista de archivos CSV en el repositorio
+#def get_csv_files(repo_url):
+#    try:
+#        # Realiza una solicitud HTTP para obtener el contenido del repositorio
+#        response = requests.get(repo_url)
+#        soup = BeautifulSoup(response.text, "html.parser")
+#        
+#        # Busca los enlaces a archivos en el repositorio
+#        links = soup.find_all("a", href=re.compile(r'\.csv$'))
+#        
+#        # Extrae los nombres de archivo de los enlaces
+#        csv_files = [link["href"] for link in links]
+#        
+#        return csv_files
+#    except Exception as e:
+#        st.error(f"Error al obtener la lista de archivos CSV: {str(e)}")
+#        return []
+#import requests
+
+# Función para obtener la lista de archivos CSV en el repositorio utilizando la GitHub API
 def get_csv_files(repo_url):
     try:
-        # Realiza una solicitud HTTP para obtener el contenido del repositorio
-        response = requests.get(repo_url)
-        soup = BeautifulSoup(response.text, "html.parser")
+        # Obtener el nombre de usuario y el nombre del repositorio desde la URL
+        parts = repo_url.split("/")
+        username = parts[-2]
+        repository = parts[-1]
         
-        # Busca los enlaces a archivos en el repositorio
-        links = soup.find_all("a", href=re.compile(r'\.csv$'))
+        # Hacer una solicitud a la GitHub API para obtener la lista de archivos
+        api_url = f"https://api.github.com/repos/{username}/{repository}/contents"
+        response = requests.get(api_url)
+        data = response.json()
         
-        # Extrae los nombres de archivo de los enlaces
-        csv_files = [link["href"] for link in links]
+        # Filtrar los archivos CSV y obtener sus nombres y URL de descarga
+        csv_files = [(item["name"], item["download_url"]) for item in data if item["name"].endswith(".csv")]
         
         return csv_files
     except Exception as e:
         st.error(f"Error al obtener la lista de archivos CSV: {str(e)}")
         return []
+
+# Obtener la lista de archivos CSV en el repositorio
+csv_files = get_csv_files(repo_url)
+
+
 
 # Obtiene la lista de archivos CSV en el repositorio
 csv_files = get_csv_files(repo_url)
