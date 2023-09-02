b8e1321d206ce7c876e2852cc4f21986bfdd03f8
SArcD <141703135+SArcD@users.noreply.github.com>
2023-09-02 17:56:42 -0600

    Update 01_GC.py

--- 01_GC.py
+++ 01_GC.py
@@ -10,24 +10,6 @@ st.title("Visualizador de Archivos CSV en GitHub")
 # URL del repositorio de GitHub
 repo_url = st.text_input("Introduce la URL del repositorio de GitHub:", "")
 
-# Función para obtener la lista de archivos CSV en el repositorio
-#def get_csv_files(repo_url):
-#    try:
-#        # Realiza una solicitud HTTP para obtener el contenido del repositorio
-#        response = requests.get(repo_url)
-#        soup = BeautifulSoup(response.text, "html.parser")
-#        
-#        # Busca los enlaces a archivos en el repositorio
-#        links = soup.find_all("a", href=re.compile(r'\.csv$'))
-#        
-#        # Extrae los nombres de archivo de los enlaces
-#        csv_files = [link["href"] for link in links]
-#        
-#        return csv_files
-#    except Exception as e:
-#        st.error(f"Error al obtener la lista de archivos CSV: {str(e)}")
-#        return []
-#import requests
 
 # Función para obtener la lista de archivos CSV en el repositorio utilizando la GitHub API
 def get_csv_files(repo_url):
@@ -49,13 +31,11 @@ def get_csv_files(repo_url):
     except Exception as e:
         st.error(f"Error al obtener la lista de archivos CSV: {str(e)}")
         return []
+#*********
+#OK
+#*********
 
 # Obtener la lista de archivos CSV en el repositorio
-csv_files = get_csv_files(repo_url)
-
-
-
-# Obtiene la lista de archivos CSV en el repositorio
 csv_files = get_csv_files(repo_url)
 
 # Muestra la lista de archivos CSV encontrados en un menú desplegable
@@ -63,8 +43,6 @@ if csv_files:
     selected_file = st.selectbox("Selecciona un archivo CSV:", csv_files)
 else:
     st.warning("No se encontraron archivos CSV en el repositorio.")
-
-# Función para cargar y mostrar el DataFrame seleccionado
 
 # Función para cargar y mostrar el DataFrame seleccionado
 def load_and_display_dataframe(selected_file):
