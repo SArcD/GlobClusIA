9693211480f1439a8d784b651b356b6facb80435
SArcD <141703135+SArcD@users.noreply.github.com>
2023-09-23 19:18:32 -0600

    Update 01_GC.py

--- 01_GC.py
+++ 01_GC.py
@@ -7,7 +7,8 @@ import requests
 st.title("Analysis of Color-Magnitude Diagrams of galactic globular clusters")
 
 st.subheader("Individual analysis")
-st.markdown("Instrucciones: Por favor selecione los archivos de fotometría ("Nombre_photo.csv") y parámetros superficiales ("Nombre_metal.csv") que correspondan al mismo cluster. Los datos para cada custer fueron obtenidos a partir de la base de datos Gaia DR3 (https://gea.esac.esa.int/archive/)")
+st.markdown("Instructions: Please select the photometry files (Name_photo.csv) and surface parameters files (Name_metal.csv) that correspond to the same cluster. The data for each cluster were obtained from the Gaia DR3 database (https://gea.esac.esa.int/archive/).")
+
 # URL del repositorio de GitHub
 repo_url = "https://github.com/SArcD/GlobClusIA"
 
