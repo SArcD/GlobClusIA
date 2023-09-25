956b0aeee62be09bc664555ca99974ed73a059f0
SArcD <141703135+SArcD@users.noreply.github.com>
2023-09-24 19:53:10 -0600

    Update 01_GC.py

--- 01_GC.py
+++ 01_GC.py
@@ -165,7 +165,16 @@ if "selected_files_tuple" in locals() and len(selected_files_tuple) >= 2:
 # Seleccionar las columnas deseadas del DataFrame original
 columnas_seleccionadas = ["source_id", "phot_g_mean_mag", "phot_bp_mean_mag", "phot_rp_mean_mag", "bp_rp", "bp_g", "g_rp", "teff_gspphot", "logg_gspphot", "mh_gspphot"]
 df_cmd = df[columnas_seleccionadas]        
-#st.dataframe(df_cmd)
+st.dataframe(df_cmd)
+# Mostrar información adicional
+st.write(f"Número de filas: {num_rows}")
+st.write(f"Número de columnas: {num_columns}")
+# Obtener el número de filas con datos faltantes
+num_rows_with_missing_data = df_cmd.isnull().any(axis=1).sum()
+        
+# Mostrar el número de filas con datos faltantes
+st.write(f"Número de filas con datos faltantes: {num_rows_with_missing_data}")
+#
 
 
 ###
