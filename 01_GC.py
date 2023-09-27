7abfe5e68f8bc571cfdbada74b0d07afc3a50425
SArcD <141703135+SArcD@users.noreply.github.com>
2023-09-26 19:58:54 -0600

    Update 01_GC.py

--- 01_GC.py
+++ 01_GC.py
@@ -817,60 +817,303 @@ st.pyplot(fig)
 ########################################
 
 import streamlit as st
+import pandas as pd
 import numpy as np
-from scipy.cluster.hierarchy import dendrogram, linkage, fcluster
 import matplotlib.pyplot as plt
+from scipy.spatial.distance import pdist
+from scipy.cluster.hierarchy import linkage, dendrogram, fcluster
+from sklearn.preprocessing import StandardScaler
+from sklearn.cluster import AgglomerativeClustering
+import plotly.express as px
 
-# Supongamos que tienes un DataFrame llamado cluster_1_data con la columna "brillo"
-#magnitudes = cluster_1_data["brillo"]
+# Eliminar filas con valores NaN
+#df_cmd = df_cmd.dropna()
+#st.dataframe(df_cmd)
+# Mostrar información adicional
+#st.write(f"Número de filas: {num_rows}")
+#st.write(f"Número de columnas: {num_columns}")
+# Obtener el número de filas con datos faltantes
+#num_rows_with_missing_data = df_cmd.isnull().any(axis=1).sum()
+        
+# Mostrar el número de filas con datos faltantes
+#st.write(f"Número de filas con datos faltantes: {num_rows_with_missing_data}")
+# Seleccionar solo las columnas numéricas
+#columnas_numericas = df_cmd.select_dtypes(include=[np.number])
+#columnas_numericas = df.select_dtypes(include=[np.number])
+#df['source_id'] = df['source_id'].astype(str)
+columnas_numericas = cluster_1_data.select_dtypes(include=['number'])
+#columnas_numericas = ["phot_g_mean_mag", "phot_bp_mean_mag", "phot_rp_mean_mag", "bp_rp", "bp_g", "g_rp", "teff_gspphot", "logg_gspphot", "mh_gspphot"]
+# Normalizar los datos (opcional, pero recomendado para clustering)
+scaler = StandardScaler()
+columnas_numericas_scaled = scaler.fit_transform(columnas_numericas)
 
-cluster_1_data=dataframes_por_cluster[1]
-magnitudes = cluster_1_data["phot_rp_mean_mag"]
+# Calcular la matriz de distancias
+dist_matrix = pdist(columnas_numericas_scaled, metric='euclidean')
+
+# Agregar una interfaz de usuario para ingresar el número deseado de clusters
 
 
+st.title("Hierarchical Clustering")
+st.write("Enter the desired number of clusters:")
 
-# Calcular la matriz de distancias euclidianas
-dist_matrix = pdist(magnitudes.values.reshape(-1, 1), metric='euclidean')
+# Agregar un campo de entrada para el número de clusters
+num_clusters = 3
 
-# Luego, puedes utilizar esta matriz de distancias en la función linkage
-enlace = linkage(dist_matrix, method='complete')
 
-# Calcular la matriz de enlace usando el método de enlace completo (complete linkage)
-#enlace = linkage(magnitudes, method='complete')
+# Calcular la matriz de enlace utilizando el método de enlace completo (complete linkage)
+Z = linkage(dist_matrix, method='ward')
 
-# Crear un dendrograma para visualizar la jerarquía de clusters
-plt.figure(figsize=(10, 6))
-dendrogram(enlace, p=5, truncate_mode='level', show_leaf_counts=True)
-plt.title("Dendrograma del Cluster")
-plt.xlabel("Índice de Muestra")
+# Realizar el clustering jerárquico aglomerativo
+clustering = AgglomerativeClustering(n_clusters=num_clusters, metric='euclidean', linkage='ward')
+cluster_labels = clustering.fit_predict(columnas_numericas_scaled)
+
+# Agregar la columna de clusters "gc" al DataFrame original
+cluster_1_data['gc'] = cluster_labels
+
+# Crear un dendrograma
+fig, ax = plt.subplots(figsize=(12, 6))
+dendrogram(Z, labels=cluster_1_data.index, leaf_rotation=90)
+plt.title("Dendrograma de Clustering Jerárquico")
+plt.xlabel("Índice de la Muestra")
 plt.ylabel("Distancia")
-st.pyplot()
+st.pyplot(fig)
+#plot_container2.plotly_chart(fig)
+st.write(f"Número de clusters seleccionado: {num_clusters}")
+st.dataframe(cluster_1_data)
+# Obtener información sobre el DataFrame
+num_rows, num_columns = cluster_1_data.shape
+num_missing = cluster_1_data.isnull().sum().sum()    
+st.write(f"**Number of rows:** {num_rows}")
+st.write(f"**Number of columns:** {num_columns}")
+    
 
-# Elegir un umbral apropiado para subdividir el cluster
-umbral = 10  # Puedes ajustar este valor según sea necesario
-
-# Aplicar la subdivisión de clusters usando el umbral
-etiquetas_clusters = fcluster(enlace, t=umbral, criterion='distance')
-
-# Filtrar los datos del DataFrame original para obtener el subcluster correspondiente al RGB-tip
-subcluster_rgb_tip = cluster_1_data[etiquetas_clusters == 3]
-
-# Calcular la posición estimada del RGB-tip en base a los datos del subcluster_rgb_tip
-posicion_rgb_tip = subcluster_rgb_tip["phot_rp_mean_mag"].mean()
-
-# Filtrar los datos del DataFrame original para obtener el subcluster correspondiente al RGB Bump
-subcluster_rgb_bump = cluster_1_data[etiquetas_clusters == 3]
-
-# Calcular la posición estimada del RGB Bump en base a los datos del subcluster_rgb_bump
-posicion_rgb_bump = subcluster_rgb_bump["phot_rp_mean_mag"].mean()
-
-# Mostrar las posiciones estimadas en Streamlit
-st.title("Estimación del RGB-tip y RGB Bump mediante Clustering Jerárquico")
-st.write(f"La posición estimada del RGB-tip es: {posicion_rgb_tip:.2f}")
-st.write(f"La posición estimada del RGB Bump es: {posicion_rgb_bump:.2f}")
+filas_con_faltantes = (cluster_1_data.isna().any(axis=1)).sum()
+# Mostrar las filas con datos faltantes
+st.write(f"**Number of rows with missing data:** {filas_con_faltantes}")
 
 
 
+######################## Diagramas de Caja ########################
+
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+import streamlit as st
+
+st.markdown(
+        """
+        The figure displays box plots that compare each of the sub-clusters formed by the hierarchical clustering technique. Each box corresponds to a group of stars (the points shown to the left of each box correspond to a specific star). The waistlines of each box are a visual aid to determine if there is enough evidence of a difference between the clusters (if the waistlines are at the same height, there is no evidence that the sub-clusters can differentiate based on their values in that variable. If they do not match in height, it can be concluded that the sub-clusters can differentiate concerning that variable."
+ """
+    )
+
+# Obtener los nombres de las columnas numéricas
+columnas_numericas= cluster_1_data.select_dtypes(include='number').drop(columns=['gc']).columns
+
+# Calcular el número de filas y columnas del panel
+num_rows = len(columnas_numericas)
+num_cols = 1  # Una columna para cada parámetro
+
+# Ajustar el espacio vertical y la altura de los subplots
+subplot_height = 400  # Ajusta la altura según tu preferencia    
+vertical_spacing = 0.004  # Ajusta el espacio vertical según tu preferencia
+
+# Crear subplots para cada parámetro
+fig = make_subplots(rows=num_rows, cols=num_cols, subplot_titles=columnas_numericas, vertical_spacing=vertical_spacing)
+
+# Crear un gráfico de caja para cada parámetro y comparar los 10 clusters
+for i, column in enumerate(columnas_numericas):
+    # Obtener los datos de cada cluster para el parámetro actual
+    cluster_data = [cluster_1_data[cluster_1_data['gc'] == cluster][column] for cluster in range(10)]
+
+    #Agregar el gráfico de caja al subplot correspondiente
+    for j in range(10):
+        box = go.Box(y=cluster_data[j], boxpoints='all', notched=True, name=f'group {j}')
+        box.hovertemplate = 'id: %{text}'  # Agregar el valor de la columna 'Nombre' al hovertemplate
+        box.text = cluster_1_data[cluster_1_data['gc'] == j]['source_id']  # Asignar los valores de la columna 'Nombre' al texto
+        fig.add_trace(box, row=i+1, col=1)
+
+#Actualizar el diseño y mostrar el panel de gráficos
+fig.update_layout(showlegend=False, height=subplot_height*num_rows, width=800,
+                      title_text='Comparación de Clusters - Gráfico de Caja',
+                      margin=dict(t=100, b=100, l=50, r=50))  # Ajustar los márgenes del layout
+
+#Mostrar la gráfica de caja en Streamlit
+st.plotly_chart(fig, use_container_width=True)
+
+
+st.markdown("""The Figure displays the box plots in which each of the clusters formed by the hierarchical clustering technique is compared. Each box corresponds to a particular cluster (where on the left side of each one, you can see the points corresponding to the contained patients). The waistlines of each box serve as a visual aid to determine if there is sufficient evidence of a difference between the clusters (if the waistlines are at the same height, there is no evidence that the clusters can differentiate based on their values in that variable. If they do not match in height, it can be concluded that the clusters can differentiate with respect to that variable).""")
+
+    ################### tsne ##############################
+
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.datasets import load_digits
+from sklearn.manifold import TSNE
+
+st.markdown("""
+        The sub-clusters represent data that, based on their values in each variable, can be considered as more similar to each other than to the rest. However, in many cases, visualizing the sub-clusters can be challenging because the number of variables involved can be very high. The techniques t-SNE and PCA can be used together to create a plot of all the points on a plane. The plot shows the points grouped within each cluster, once the techniques of Principal Component Analysis (PCA) and t-Distributed Stochastic Neighbor Embedding (t-SNE) have been applied. The contours around each cluster correspond to the density of points (where the lines are more concentrated, it signifies a higher point density).""")
+
+
+numeric_data=cluster_1_data.select_dtypes(include='number')
+m = TSNE(learning_rate=100)
+# Ajustar y transformar el modelo de t-SNE en el conjunto de datos numéricos
+tsne_features = m.fit_transform(numeric_data.drop(['gc'],axis=1))
+
+import pandas as pd
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+
+# Normalizar los datos
+scaler = StandardScaler()
+normalized_data = scaler.fit_transform(numeric_data)
+# Crear una instancia de PCA
+pca = PCA()
+# Aplicar PCA a los datos normalizados
+pca_data = pca.fit_transform(normalized_data)
+# Crear un nuevo DataFrame con las componentes principales
+pca_df = pd.DataFrame(data=pca_data, columns=[f'PC{i}' for i in range(1, pca.n_components_+1)])
+
+cluster_1_data = cluster_1_data.reset_index(drop=True)
+# Reset the indices of the DataFrames
+pca_df.reset_index(drop=True, inplace=True)
+cluster_1_data.reset_index(drop=True, inplace=True)
+pca_df = pd.concat([pca_df, cluster_1_data["source_id"]], axis=1)
+
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.manifold import TSNE
+from scipy.stats import gaussian_kde
+
+    #data=df_cmd
+labels = cluster_1_data['gc']
+# Crear una instancia de t-SNE con los hiperparámetros deseados
+tsne = TSNE(n_components=2, perplexity=40, early_exaggeration=10, learning_rate=5)
+
+# Ajustar t-SNE a los datos de PCA
+tsne_data = tsne.fit_transform(pca_data)
+
+# Crear una figura y un eje
+fig, ax = plt.subplots()
+
+# Colorear los puntos según las agrupaciones originales
+for gc in np.unique(labels):
+    indices = np.where(labels == gc)
+    ax.scatter(tsne_data[indices, 0], tsne_data[indices, 1], label=f'Group {gc}')
+
+    # Estimar la densidad de los puntos en el cluster actual
+    kde = gaussian_kde(tsne_data[indices].T)
+    x_range = np.linspace(np.min(tsne_data[:, 0]-1), np.max(tsne_data[:, 0]+1), 100)
+    y_range = np.linspace(np.min(tsne_data[:, 1]-1), np.max(tsne_data[:, 1]+1), 100)
+    xx, yy = np.meshgrid(x_range, y_range)
+    positions = np.vstack([xx.ravel(), yy.ravel()])
+    zz = np.reshape(kde(positions).T, xx.shape)
+
+    # Agregar las curvas de densidad de kernel al gráfico
+    ax.contour(xx, yy, zz, colors='k', alpha=0.5)
+
+# Agregar leyenda y título al gráfico
+ax.legend()
+ax.set_title('Gráfico de Dispersión de t-SNE con Curvas de Densidad de Kernel')
+
+# Mostrar el gráfico
+plt.show()
+
+import numpy as np
+import plotly.graph_objects as go
+from sklearn.manifold import TSNE
+from scipy.stats import gaussian_kde
+
+
+
+# Crear una figura
+fig = go.Figure()
+cluster_colors = ['blue', 'cyan', 'red', 'pink', 'green']
+# Estimar la densidad de los puntos en cada cluster y agregar la superficie de contorno correspondiente
+for gc in np.unique(labels):
+    indices = np.where(labels == gc)
+
+    kde = gaussian_kde(tsne_data[indices].T)
+    x_range = np.linspace(np.min(tsne_data[:, 0])-5, np.max(tsne_data[:, 0])+5, 100)
+    y_range = np.linspace(np.min(tsne_data[:, 1])-5, np.max(tsne_data[:, 1])+5, 100)
+    xx, yy = np.meshgrid(x_range, y_range)
+    positions = np.vstack([xx.ravel(), yy.ravel()])
+    zz = np.reshape(kde(positions).T, xx.shape)
+
+    if gc in [0]:
+        opacity = 0.9
+        levels = 10
+    elif gc ==1:
+        opacity = 0.5
+        levels = 7
+    else:
+        opacity = 0.3
+        levels = 5
+    
+    contour_trace = go.Contour(
+        x=x_range,
+        y=y_range,
+        z=zz,
+        colorscale='Blues',
+        opacity=opacity,
+        showscale=False,
+        name=f'Contour {gc}'
+    )
+    fig.add_trace(contour_trace)
+
+# Colorear los puntos según las agrupaciones originales
+for gc in np.unique(labels):
+    indices = np.where(labels == gc)
+
+    scatter_trace = go.Scatter(
+        x=tsne_data[indices, 0].flatten(),
+        y=tsne_data[indices, 1].flatten(),
+        mode='markers',
+        text=cluster_1_data.loc[labels == gc, ["source_id", "phot_g_mean_mag", "phot_bp_mean_mag", "phot_rp_mean_mag", "bp_rp", "bp_g", "g_rp", "teff_gspphot", "logg_gspphot", "mh_gspphot"]].apply(lambda x: '<br>'.join(x.astype(str)), axis=1),
+        hovertemplate="%{text}",
+        marker=dict(
+            size=7,
+            line=dict(width=0.5, color='black')
+        ),
+        name=f'Cluster {gc}'
+    )
+    fig.add_trace(scatter_trace)
+
+# Configurar el diseño del gráfico con el ancho de pantalla ajustado
+fig.update_layout(
+    title='Gráfico de Dispersión de t-SNE con Curvas de Densidad de Kernel',
+    xaxis_title='Dimensión 1',
+    yaxis_title='Dimensión 2',
+    showlegend=True,
+    legend_title='Clusters',
+    width=1084  # Ajustar el ancho del gráfico
+)
+# Mostrar el gráfico
+st.plotly_chart(fig, use_container_width=True)
+
+
+    ###
+
+import pandas as pd
+import plotly.express as px
+
+# Gráfica 1: phot_g_mean_mag vs g_rp
+fig1 = px.scatter(cluster_1_data, x="g_rp", y="phot_g_mean_mag", color="gc",
+                        hover_data=cluster_1_data.columns, title="phot_g_mean_mag vs g_rp")
+fig1.update_yaxes(autorange="reversed")
+
+# Gráfica 2: phot_bp_mean_mag vs bp_rp
+fig2 = px.scatter(cluster_1_data, x="bp_rp", y="phot_bp_mean_mag", color="gc",
+                    hover_data=cluster_1_data.columns, title="phot_bp_mean_mag vs bp_rp")
+fig2.update_yaxes(autorange="reversed")
+
+# Gráfica 3: phot_rp_mean_mag vs bp_rp
+fig3 = px.scatter(cluster_1_data, x="bp_rp", y="phot_rp_mean_mag", color="gc",
+                    hover_data=cluster_1_data.columns, title="phot_rp_mean_mag vs bp_rp")
+fig3.update_yaxes(autorange="reversed")
+
+# Mostrar las gráficas en Streamlit
+st.plotly_chart(fig1)
+st.plotly_chart(fig2)
+st.plotly_chart(fig3)
 
 
 
